4576317244c19e97a8eab33035377f73fb7c3ef8
Hamza Ashfaq <hamza.ashfaque@epesys.com>
2023-09-04 18:49:22 +0500

    ver1

---
+++ wayfair/scraper.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import csv
+import uuid
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome()
